3940d535c545049464616100a1769e55d92a844c
Tomás Martins <tomas.marti@me.com>
2023-02-20 11:13:41 -0300

    Updated SBKHttpMethod

--- Sources/ScrobbleKit/Helpers/SBKHttpMethod.swift
+++ Sources/ScrobbleKit/Helpers/SBKHttpMethod.swift
@@ -10,4 +10,11 @@ import Foundation
 public enum SBKHttpMethod: String {
     case `get` = "GET"
     case post = "POST"
+    case put = "PUT"
+    case patch = "PATCH"
+    case delete = "DELETE"
+    case head = "HEAD"
+    case options = "OPTIONS"
+    case trace = "TRACE"
+    case connect = "CONNECT"
 }
