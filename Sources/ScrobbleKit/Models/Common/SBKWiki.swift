603f8b03820a43f97dbf129858cd9be74e4cdb37
Tomás Martins <tomas.marti@me.com>
2023-02-20 19:07:44 -0300

    SBWIki doc

--- Sources/ScrobbleKit/Models/Common/SBKWiki.swift
+++ Sources/ScrobbleKit/Models/Common/SBKWiki.swift
@@ -7,11 +7,25 @@
 
 import Foundation
 
+/**
+ A model representing a wiki entry.
+ */
 public struct SBKWiki {
+    
+    /// The summary of the wiki entry.
     public let summary: String
+    
+    /// The content of the wiki entry.
     public let content: String
+    
+    /// The date and time the wiki entry was published, or `nil` if the date couldn't be parsed.
     public let published: Date?
     
+    /**
+     Initializes a new `SBKWiki` instance based on a `SBKWikiResponse`.
+     
+     - Parameter response: A `SBKWikiResponse` instance representing the wiki entry.
+     */
     internal init(response: SBKWikiResponse) {
         self.summary = response.summary
         self.content = response.content
@@ -21,3 +35,4 @@ public struct SBKWiki {
         self.published = dateFormatter.date(from: response.published)
     }
 }
+
