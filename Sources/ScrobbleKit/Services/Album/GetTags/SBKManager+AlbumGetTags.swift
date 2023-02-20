daa171449d48cca544a7c3b497dc31461965e6e6
Tomás Martins <tomas.marti@me.com>
2023-02-20 18:28:50 -0300

    album.getTags doc

--- Sources/ScrobbleKit/Services/Album/GetTags/SBKManager+AlbumGetTags.swift
+++ Sources/ScrobbleKit/Services/Album/GetTags/SBKManager+AlbumGetTags.swift
@@ -8,6 +8,22 @@
 import Foundation
 
 extension SBKManager {
+    /**
+     Gets the tags applied by an individual user to an album on Last.fm.
+     
+     To retrieve the list of tags applied to an album by all users use ``getTopTags(forAlbum:autoCorrect:)``
+
+     - Parameters:
+        - searchMethod: The album search method, either by name or by a MusicBrainz ID.
+        - autoCorrect: A Boolean value indicating whether to transform misspelled artist names into correct artist names. The default value is `true`.
+        - username: The username for the context of the request. If supplied, the tags of this album applied by the user are included in the response.
+     
+     - Returns: An array of ``SBKTag`` objects representing the tags applied by the user to the album.
+     
+     - Throws: An error of type ``SBKClientError``or ``SBKError`` if the operation fails
+     
+     - Note: See [Last.fm's album.addTags documentation](https://www.last.fm/api/show/album.getTags) for more information.
+     */
     public func getTags(
         forAlbum searchMethod: SBKAlbumSearchMethod,
         autoCorrect: Bool = true,
@@ -24,6 +40,21 @@ extension SBKManager {
         return response.tags.tag
     }
     
+    /**
+     Gets the tags applied by an individual user to an album on Last.fm.
+     
+     To retrieve the list of tags applied to an album by all users use ``getTopTags(forAlbum:autoCorrect:_:)``
+     
+     - Parameters:
+        - searchMethod: The album search method, either by name or by MusicBrainz ID.
+        - autoCorrect: A Boolean value indicating whether to transform misspelled artist names into correct artist names. The default value is `true`.
+        - username: The username for the context of the request. If supplied, the tags of this album applied by the user are included in the response.
+        - completion: A closure to be called with the result of the operation. The closure takes an optional array of ``SBKTag`` objects and an optional error object as its parameters.
+     
+     - Throws: An error of type ``SBKClientError``or ``SBKError`` if the operation fails
+     
+     - Note: See [Last.fm's album.addTags documentation](https://www.last.fm/api/show/album.getTags) for more information.
+     */
     public func getTags(forAlbum searchMethod: SBKAlbumSearchMethod,
                  autoCorrect: Bool = true,
                  username: String? = nil,
