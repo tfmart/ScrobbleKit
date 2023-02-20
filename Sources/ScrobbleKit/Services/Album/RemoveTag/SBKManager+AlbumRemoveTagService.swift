a17949e2db840210248d0f49014277f0004cc2b7
Tomás Martins <tomas.marti@me.com>
2023-02-20 18:45:30 -0300

    album.removeTag doc

--- Sources/ScrobbleKit/Services/Album/RemoveTag/SBKManager+AlbumRemoveTagService.swift
+++ Sources/ScrobbleKit/Services/Album/RemoveTag/SBKManager+AlbumRemoveTagService.swift
@@ -8,6 +8,17 @@
 import Foundation
 
 public extension SBKManager {
+    /**
+      Remove a user's tag from an album on Last.fm.
+
+      - Parameter album: The name of the album.
+      - Parameter artist: The name of the album's artist.
+      - Parameter tag: The tag to remove from the album.
+      - Returns: A Boolean value indicating whether the tag was removed from the album.
+      - Throws: An error of type ``SBKClientError``or ``SBKError`` if the operation fails
+
+      For more information, see the [Last.fm API documentation](https://www.last.fm/api/show/album.removeTag).
+    */
     func removeTag(fromAlbum album: String, artist: String, tag: String) async throws -> Bool {
         guard let sessionKey else { throw SBKClientError.missingSessionKey }
         let service = AlbumRemoveTagService(
@@ -22,9 +33,19 @@ public extension SBKManager {
         return true
     }
     
-    func removeTag(fromAlbum album: String, artist: String, tag: String, _ completion: ((Bool?, Error?) -> Void)?) {
+    /**
+      Remove a user's tag from an album on Last.fm API.
+
+      - Parameter album: The name of the album.
+      - Parameter artist: The name of the artist.
+      - Parameter tag: The tag to remove from the album.
+      - Parameter completion: A completion block to be called with the result of the operation.
+
+      For more information, see the [Last.fm API documentation](https://www.last.fm/api/show/album.removeTag).
+    */
+    func removeTag(fromAlbum album: String, artist: String, tag: String, _ completion: ((Bool, Error?) -> Void)?) {
         guard let sessionKey else {
-            completion?(nil, SBKClientError.missingSessionKey)
+            completion?(false, SBKClientError.missingSessionKey)
             return
         }
         let service = AlbumRemoveTagService(
@@ -37,11 +58,11 @@ public extension SBKManager {
         )
         service.start { response, error in
             guard error == nil else {
-                completion?(nil, error)
+                completion?(false, error)
                 return
             }
             guard response != nil else {
-                completion?(nil, SBKClientError.failedToDecodeResponse)
+                completion?(false, SBKClientError.failedToDecodeResponse)
                 return
             }
             completion?(true, nil)
