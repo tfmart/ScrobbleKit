601fb5fad9dc8bdb322c1babf64b5138f89240c0
Tomás Martins <tomas.marti@me.com>
2023-02-20 19:11:30 -0300

    SBKTag doc

--- Sources/ScrobbleKit/Models/Common/SBKTag.swift
+++ Sources/ScrobbleKit/Models/Common/SBKTag.swift
@@ -7,7 +7,15 @@
 
 import Foundation
 
+/**
+ A struct representing a tag returned from a Last.fm API call.
+ */
 public struct SBKTag: Decodable {
+    
+    /// The URL for the tag on the Last.fm website.
     public let url: String
+    
+    /// The name of the tag.
     public let name: String
 }
+
