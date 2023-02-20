0bdd6c5d531f5488579ac5c1d6e0b635f9ae89ff
Tomás Martins <tomas.marti@me.com>
2023-02-20 18:15:15 -0300

    Added doc for addTags(forAlbum:)

--- Sources/ScrobbleKit/Services/Album/AddTags/SBKManager+AlbumAddTags.swift
+++ Sources/ScrobbleKit/Services/Album/AddTags/SBKManager+AlbumAddTags.swift
@@ -8,6 +8,18 @@
 import Foundation
 
 public extension SBKManager {
+    /**
+     Tag an album using a list of user-supplied tags.
+     
+     - Parameters:
+       - artist: The name of the artist.
+       - album: The name of the album.
+       - tags: A list of user-supplied tags to apply to this album. **Accepts a maximum of 10 tags.**
+     
+     - Returns: A boolean value indicating whether the operation was successful.
+     
+     - Note: See [Last.fm's album.addTags documentation](https://www.last.fm/api/show/album.addTags) for more information.
+     */
     @discardableResult
     func addTags(toAlbum album: String, artist: String, tags: [String]) async throws -> Bool {
         guard let sessionKey else { throw SBKClientError.missingSessionKey }
@@ -23,6 +35,17 @@ public extension SBKManager {
         return true
     }
     
+    /**
+     Tag an album using a list of user-supplied tags.
+     
+     - Parameters:
+       - artist: The name of the artist.
+       - album: The name of the album.
+       - tags: A list of user-supplied tags to apply to this album. Accepts a maximum of 10 tags.
+       - completion: The completion handler to call when the request is complete.
+     
+     - Note: See [Last.fm's album.addTags documentation](https://www.last.fm/api/show/album.addTags) for more information.
+     */
     func addTags(toAlbum album: String, artist: String, tags: [String], _ completion: ((Bool?, Error?) -> Void)?) {
         guard let sessionKey else {
             completion?(nil, SBKClientError.missingSessionKey)
