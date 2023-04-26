c1dc159319f2fea4f16c7696e19fb1c80123a05a
Tomás Martins <tomas.marti@me.com>
2023-04-26 00:59:29 -0300

    Added TrackCorrectionService

--- Sources/ScrobbleKit/Services/Track/GetCorrection/TrackCorrectionService.swift
+++ Sources/ScrobbleKit/Services/Track/GetCorrection/TrackCorrectionService.swift
@@ -1,29 +1,29 @@
-////
-////  TrackCorrectionService.swift
-////
-////
-////  Created by Tomas Martins on 26/04/23.
-////
 //
-//import Foundation
+//  TrackCorrectionService.swift
 //
-//struct TrackCorrectionService: SBKService {
-//    typealias ResponseType = SBKTrackCorrectionResponse
 //
-//    var sbkMethod: SBKMethod = .getCorrectedTrackInfo
-//    var queries: [URLQueryItem]
-//    var httpMethod: SBKHttpMethod = .geta
+//  Created by Tomas Martins on 26/04/23.
 //
-//    var apiKey: String
-//    var secretKey: String?
-//
-//    init(trackName: String, artistName: String, apiKey: String, secretKey: String? = nil) {
-//        self.apiKey = apiKey
-//        self.secretKey = secretKey
-//
-//        queries = [
-//            .init(name: "track", value: trackName),
-//            .init(name: "artist", value: artistName)
-//        ]
-//    }
-//}
+
+import Foundation
+
+struct TrackCorrectionService: SBKService {
+    typealias ResponseType = SBKCorrectedResult<SBKTrack>
+
+    var sbkMethod: SBKMethod = .getCorrectedTrackInfo
+    var queries: [URLQueryItem]
+    var httpMethod: SBKHttpMethod = .get
+
+    var apiKey: String
+    var secretKey: String
+
+    init(trackName: String, artistName: String, apiKey: String, secretKey: String) {
+        self.apiKey = apiKey
+        self.secretKey = secretKey
+
+        queries = [
+            .init(name: "track", value: trackName),
+            .init(name: "artist", value: artistName)
+        ]
+    }
+}
