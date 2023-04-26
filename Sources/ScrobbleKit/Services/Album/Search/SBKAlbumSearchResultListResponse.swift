abfae9d975fd74368157ba99a55a532726042654
Tomás Martins <tomas.marti@me.com>
2023-04-26 00:12:34 -0300

    Added SBKAlbumSearchResultListResponse

---
+++ Sources/ScrobbleKit/Services/Album/Search/SBKAlbumSearchResultListResponse.swift
@@ -0,0 +1,12 @@
+//
+//  SBKAlbumSearchResultListResponse.swift
+//  
+//
+//  Created by Tomas Martins on 26/04/23.
+//
+
+import Foundation
+
+struct SBKAlbumSearchResultListResponse: Decodable {
+    var album: [SBKAlbum]?
+}
