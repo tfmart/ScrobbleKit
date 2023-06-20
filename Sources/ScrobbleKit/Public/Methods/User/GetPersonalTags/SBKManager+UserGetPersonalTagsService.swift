98471f0b1e1ba39ca920e063dee6a5b9f74d1a3e
Tomás Martins <tomas.marti@me.com>
2023-06-20 15:47:48 -0300

    Added documentation and completion based methods for track.getPersonalTags

--- Sources/ScrobbleKit/Public/Methods/User/GetPersonalTags/SBKManager+UserGetPersonalTagsService.swift
+++ Sources/ScrobbleKit/Public/Methods/User/GetPersonalTags/SBKManager+UserGetPersonalTagsService.swift
@@ -8,6 +8,21 @@
 import Foundation
 
 public extension SBKManager {
+    /**
+     Get personal tags for artists from a user on Last.fm.
+     
+     - Parameters:
+       - user: The username of the user.
+       - tag: The tag you're interested in.
+       - limit: The number of results to fetch per page. Defaults to 50.
+       - page: The page number to fetch. Defaults to first page.
+     
+     - Returns: An asynchronous task that returns an `SBKSearchResult` containing an array of `SBKArtist` objects.
+     
+     - Throws: An error of type `SBKError` if the operation fails.
+     
+     - Note: See [Last.fm's user.getPersonalTags documentation](https://www.last.fm/api/show/user.getPersonalTags) for more information.
+     */
     func getPersonalTagsForArtists(fromUser user: String, tag: String, limit: Int = 50, page: Int = 1) async throws -> SBKSearchResult<SBKArtist> {
         let service = UserGetPersonalTagsService(
             user: user,
@@ -37,6 +52,21 @@ public extension SBKManager {
                      total: total)
     }
     
+    /**
+     Get personal tags for tracks from a user on Last.fm.
+     
+     - Parameters:
+       - user: The username of the user.
+       - tag: The tag you're interested in.
+       - limit: The number of results to fetch per page. Defaults to 50.
+       - page: The page number to fetch. Defaults to first page.
+     
+     - Returns: An asynchronous task that returns an `SBKSearchResult` containing an array of `SBKTrack` objects.
+     
+     - Throws: An error of type `SBKError` if the operation fails.
+     
+     - Note: See [Last.fm's user.getPersonalTags documentation](https://www.last.fm/api/show/user.getPersonalTags) for more information.
+     */
     func getPersonalTagsForTracks(fromUser user: String, tag: String, limit: Int = 50, page: Int = 1) async throws -> SBKSearchResult<SBKTrack> {
         let service = UserGetPersonalTagsService(
             user: user,
@@ -66,6 +96,21 @@ public extension SBKManager {
                      total: total)
     }
     
+    /**
+     Get personal tags for albums from a user on Last.fm.
+     
+     - Parameters:
+       - user: The username of the user.
+       - tag: The tag you're interested in.
+       - limit: The number of results to fetch per page. Defaults to 50.
+       - page: The page number to fetch. Defaults to first page.
+     
+     - Returns: An asynchronous task that returns an `SBKSearchResult` containing an array of `SBKAlbum` objects.
+     
+     - Throws: An error of type `SBKError` if the operation fails.
+     
+     - Note: See [Last.fm's user.getPersonalTags documentation](https://www.last.fm/api/show/user.getPersonalTags) for more information.
+     */
     func getPersonalTagsForAlbums(fromUser user: String, tag: String, limit: Int = 50, page: Int = 1) async throws -> SBKSearchResult<SBKAlbum> {
         let service = UserGetPersonalTagsService(
             user: user,
@@ -93,5 +138,74 @@ public extension SBKManager {
                      perPage: perPage,
                      totalPages: totalPages,
                      total: total)
+    }
+    
+    /**
+     Get personal tags for artists from a user on Last.fm.
+     
+     - Parameters:
+       - user: The username of the user.
+       - tag: The tag you're interested in.
+       - limit: The number of results to fetch per page. Defaults to 50.
+       - page: The page number to fetch. Defaults to first page.
+       - completion: The completion handler to call when the request is complete. It returns an `SBKSearchResult` containing an array of `SBKArtist` objects and an optional error.
+     
+     - Note: See [Last.fm's user.getPersonalTags documentation](https://www.last.fm/api/show/user.getPersonalTags) for more information.
+     */
+    func getPersonalTagsForArtists(fromUser user: String, tag: String, limit: Int = 50, page: Int = 1, completion: ((SBKSearchResult<SBKArtist>?, Error?) -> Void)?) {
+        Task {
+            do {
+                let result = try await getPersonalTagsForArtists(fromUser: user, tag: tag, limit: limit, page: page)
+                completion?(result, nil)
+            } catch {
+                completion?(nil, error)
+            }
+        }
+    }
+    
+    /**
+     Get personal tags for tracks from a user on Last.fm.
+     
+     - Parameters:
+       - user: The username of the user.
+       - tag: The tag you're interested in.
+       - limit: The number of results to fetch per page. Defaults to 50.
+       - page: The page number to fetch. Defaults to first page.
+       - completion: The completion handler to call when the request is complete. It returns an `SBKSearchResult` containing an array of `SBKTrack` objects and an optional error.
+     
+     - Note: See [Last.fm's user.getPersonalTags documentation](https://www.last.fm/api/show/user.getPersonalTags) for more information.
+     */
+    func getPersonalTagsForTracks(fromUser user: String, tag: String, limit: Int = 50, page: Int = 1, completion: ((SBKSearchResult<SBKTrack>?, Error?) -> Void)?) {
+        Task {
+            do {
+                let result = try await getPersonalTagsForTracks(fromUser: user, tag: tag, limit: limit, page: page)
+                completion?(result, nil)
+            } catch {
+                completion?(nil, error)
+            }
+        }
+    }
+    
+    /**
+     Get personal tags for albums from a user on Last.fm.
+     
+     - Parameters:
+       - user: The username of the user.
+       - tag: The tag you're interested in.
+       - limit: The number of results to fetch per page. Defaults to 50.
+       - page: The page number to fetch. Defaults to first page.
+       - completion: The completion handler to call when the request is complete. It returns an `SBKSearchResult` containing an array of `SBKAlbum` objects and an optional error.
+     
+     - Note: See [Last.fm's user.getPersonalTags documentation](https://www.last.fm/api/show/user.getPersonalTags) for more information.
+     */
+    func getPersonalTagsForAlbums(fromUser user: String, tag: String, limit: Int = 50, page: Int = 1, completion: ((SBKSearchResult<SBKAlbum>?, Error?) -> Void)?) {
+        Task {
+            do {
+                let result = try await getPersonalTagsForAlbums(fromUser: user, tag: tag, limit: limit, page: page)
+                completion?(result, nil)
+            } catch {
+                completion?(nil, error)
+            }
+        }
     }
 }
---
+++ Sources/ScrobbleKit/Public/Models/SBKSearchAttributes.swift
@@ -0,0 +1,17 @@
+//
+//  SBKSearchAttributes.swift
+//  
+//
+//  Created by Tomas Martins on 16/06/23.
+//
+
+import Foundation
+
+
+public struct SBKSearchAttributes {
+    let term: String
+    let totalPages: String
+    let page: String
+    let perPage: String
+    let total: String
+}
