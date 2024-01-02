f769e10092ed938b4bc037cb8e80ecdf716775a4
Tomás Martins <tomas.marti@me.com>
2024-01-02 16:01:34 -0300

    Added doc for SBKSearchAttributes

--- Sources/ScrobbleKit/Public/Models/SBKSearchAttributes.swift
+++ Sources/ScrobbleKit/Public/Models/SBKSearchAttributes.swift
@@ -1,17 +1,22 @@
 //
 //  SBKSearchAttributes.swift
-//  
+//
 //
 //  Created by Tomas Martins on 16/06/23.
 //
 
 import Foundation
 
-
+/// Represents search attributes used in Last.fm search queries
 public struct SBKSearchAttributes {
+    /// The search term used in the query.
     public let term: String
+    /// The total number of pages available.
     public let totalPages: String
+    /// The current page number.
     public let page: String
+    /// The number of results per page.
     public let perPage: String
+    /// The total number of results found.
     public let total: String
 }
