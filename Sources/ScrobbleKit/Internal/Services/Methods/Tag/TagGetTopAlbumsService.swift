b8fe0bebb2b0de7e7213fac3111142f9cfa3e613
Tomás Martins <tomas.marti@me.com>
2024-08-04 15:49:02 -0300

    Added tag.getTopAlbums method

---
+++ Sources/ScrobbleKit/Internal/Services/Methods/Tag/TagGetTopAlbumsService.swift
@@ -0,0 +1,40 @@
+//
+//  TagGetTopAlbumsService.swift
+//  ScrobbleKit
+//
+//  Created by Tomas Martins on 04/08/24.
+//
+
+import Foundation
+
+struct TagGetTopAlbumsService: SBKService {
+    typealias ResponseType = TagTopAlbumsResponse
+    
+    let tag: String
+    let limit: Int
+    let page: Int
+    var apiKey: String
+    var secretKey: String
+    
+    var sbkMethod: SBKMethod = .tagTopAlbums
+    var queries: [URLQueryItem]
+    var httpMethod: SBKHttpMethod = .get
+    
+    init(tag: String, limit: Int, page: Int, apiKey: String, secretKey: String) {
+        self.tag = tag
+        self.limit = limit
+        self.page = page
+        self.apiKey = apiKey
+        self.secretKey = secretKey
+        
+        self.queries = [
+            .init(name: "tag", value: tag),
+            .init(name: "limit", value: "\(limit)"),
+            .init(name: "page", value: "\(page)")
+        ]
+    }
+}
+
+struct TagTopAlbumsResponse: Decodable {
+    let albums: [SBKTaggedItem]
+}
---
+++ Sources/ScrobbleKit/Public/Methods/Tag/SBKManager+TagGetTopAlbums.swift
@@ -0,0 +1,31 @@
+//
+//  SBKManager+TagGetTopAlbums.swift
+//  ScrobbleKit
+//
+//  Created by Tomas Martins on 04/08/24.
+//
+
+
+import Foundation
+
+public extension SBKManager {
+    /**
+     Get the top albums tagged by this tag, ordered by tag count.
+     
+     - Parameters:
+        - tag: The tag name.
+        - limit: The number of results to fetch per page. Defaults to 50.
+        - page: The page number to fetch. Defaults to first page.
+     
+     - Returns: An array of `SBKTaggedItem` representing the top albums.
+     
+     - Throws: An error of type `SBKError` if the request fails.
+     
+     - Note: For more information, see the [Last.fm API documentation](https://www.last.fm/api/show/tag.getTopAlbums).
+     */
+    func getTopAlbums(forTag tag: String, limit: Int = 50, page: Int = 1) async throws -> [SBKTaggedItem] {
+        let service = TagGetTopAlbumsService(tag: tag, limit: limit, page: page, apiKey: apiKey, secretKey: secret)
+        let response = try await service.start()
+        return response.albums
+    }
+}
