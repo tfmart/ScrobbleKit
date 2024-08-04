d994bffab8990746285fce6a03407aba93dbba99
Tomás Martins <tomas.marti@me.com>
2024-08-04 14:49:15 -0300

    Added documentation to SBKTrackToScrobble

--- Sources/ScrobbleKit/Public/Models/SBKTrackToScrobble.swift
+++ Sources/ScrobbleKit/Public/Models/SBKTrackToScrobble.swift
@@ -7,18 +7,41 @@
 
 import Foundation
 
+/// Represents a track to be scrobbled to Last.fm.
 public struct SBKTrackToScrobble {
+    /// The artist name.
     public let artist: String
+    /// The track name.
     public let track: String
+    /// The time the track started playing, in UTC.
     public let timestamp: Date
+    /// The album name (optional).
     public let album: String?
+    /// The album artist - if this differs from the track artist (optional).
     public let albumArtist: String?
+    /// The track number of the track on the album (optional).
     public let trackNumber: Int?
+    /// The length of the track in seconds (optional).
     public let duration: Int?
+    /// The stream id for this track received from the radio.getPlaylist service, if scrobbling Last.fm radio (optional).
     public let streamId: String?
+    /// Whether the user chose this song (optional). If not specified, assumes the user chose the song.
     public let chosenByUser: Bool?
+    /// The MusicBrainz Track ID (optional).
     public let mbid: String?
 
+    /// Initializes a new track to be scrobbled.
+    /// - Parameters:
+    ///   - artist: The artist name.
+    ///   - track: The track name.
+    ///   - timestamp: The time the track started playing, in UTC.
+    ///   - album: The album name (optional).
+    ///   - albumArtist: The album artist, if different from the track artist (optional).
+    ///   - trackNumber: The track number on the album (optional).
+    ///   - duration: The length of the track in seconds (optional).
+    ///   - streamId: The stream id for the track if scrobbling Last.fm radio (optional).
+    ///   - chosenByUser: Whether the user chose this song (optional).
+    ///   - mbid: The MusicBrainz Track ID (optional).
     public init(
         artist: String,
         track: String,
