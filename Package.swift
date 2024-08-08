032c28fc6efc8566af7c32110f4711f863910a8f
Tomás Martins <tomas.marti@me.com>
2024-08-08 13:44:27 -0300

    Updated docc dependency

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
             targets: ["ScrobbleKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-docc-plugin", branch: "main")
+        .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
