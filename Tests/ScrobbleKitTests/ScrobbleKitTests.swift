48a24b95cdda13a57afb9ed0c62cf45df242dff7
Tomás Martins <tomas.marti@me.com>
2024-08-08 13:40:45 -0300

    Added SBKManager tests

--- Tests/ScrobbleKitTests/ScrobbleKitTests.swift
+++ Tests/ScrobbleKitTests/ScrobbleKitTests.swift
@@ -9,7 +9,27 @@ import XCTest
 @testable import ScrobbleKit
 
 final class ScrobbleKitTests: XCTestCase {
-    func testExample() async throws {
-        // ...
+    var manager: SBKManager!
+    
+    override func setUp() {
+        super.setUp()
+        manager = SBKManager(apiKey: "test_api_key", secret: "test_secret_key")
+    }
+    
+    override func tearDown() {
+        manager = nil
+        super.tearDown()
+    }
+    
+    func testInitialization() {
+        XCTAssertNotNil(manager)
+        XCTAssertEqual(manager.apiKey, "test_api_key")
+        XCTAssertEqual(manager.secret, "test_secret_key")
+    }
+    
+    func testSetSessionKey() {
+        let sessionKey = "test_session_key"
+        manager.setSessionKey(sessionKey)
+        XCTAssertEqual(manager.sessionKey, sessionKey)
     }
 }
