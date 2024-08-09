48f82a4696ca758877e73296795607a1eaedd386
Tomás Martins <tomas.marti@me.com>
2024-08-09 19:40:31 -0300

    Added missing docs to public methods

--- Sources/ScrobbleKit/Public/Methods/Artist/RemoveTag/SBKManager+ArtistRemoveTagService.swift
+++ Sources/ScrobbleKit/Public/Methods/Artist/RemoveTag/SBKManager+ArtistRemoveTagService.swift
@@ -9,6 +9,21 @@ import Foundation
 
 public extension SBKManager {
 
+    /**
+     Remove a user's tag from an artist on Last.fm.
+     
+     - Parameters:
+        - artist: The name of the artist to remove the tag from.
+        - tag: The tag to remove.
+     
+     - Returns: A boolean value indicating whether the tag was successfully removed.
+     
+     - Throws: An error of type ``SBKClientError`` or ``SBKError`` if there is an issue with the API request or the user is not authenticated.
+     
+     - Note: This method requires user authentication. Make sure the user is logged in before calling this method.
+     For more information, see the [Last.fm API documentation](https://www.last.fm/api/show/artist.removeTag).
+     */
+    @discardableResult
     func removeTag(fromArtist artist: String, tag: String) async throws -> Bool {
         guard let sessionKey else { throw SBKClientError.missingSessionKey }
         let service = RemoveTagService(
@@ -22,6 +37,17 @@ public extension SBKManager {
         return true
     }
     
+    /**
+     Remove a user's tag from an artist on Last.fm.
+     
+     - Parameters:
+        - artist: The name of the artist to remove the tag from.
+        - tag: The tag to remove.
+        - completion: A closure to be called when the request is complete. It returns a boolean indicating success and an optional error.
+     
+     - Note: This method requires user authentication. Make sure the user is logged in before calling this method.
+     For more information, see the [Last.fm API documentation](https://www.last.fm/api/show/artist.removeTag).
+     */
     func removeTag(fromArtist artist: String, tag: String, _ completion: ((Bool, Error?) -> Void)?) {
         guard let sessionKey else {
             completion?(false, SBKClientError.missingSessionKey)
@@ -47,4 +73,3 @@ public extension SBKManager {
         }
     }
 }
-
--- Sources/ScrobbleKit/Public/Methods/Geo/GetTopTracks/SBKManager+GeoGetTopTracksService.swift
+++ Sources/ScrobbleKit/Public/Methods/Geo/GetTopTracks/SBKManager+GeoGetTopTracksService.swift
@@ -8,6 +8,20 @@
 import Foundation
 
 public extension SBKManager {
+    /**
+     Get the most popular tracks on Last.fm by country.
+     
+     - Parameters:
+        - country: A country name, as defined by the ISO 3166-1 country names standard.
+        - limit: The number of results to fetch per page. Defaults to 50.
+        - page: The page number to fetch. Defaults to 1.
+     
+     - Returns: An array of ``SBKTrack`` objects representing the top tracks for the specified country.
+     
+     - Throws: An error of type ``SBKClientError`` or ``SBKError`` if there is an issue with the API request or decoding the response.
+     
+     - Note: For more information, see the [Last.fm API documentation](https://www.last.fm/api/show/geo.getTopTracks).
+     */
     func getTopTracks(
         forCountry country: SBKCountry,
         limit: Int = 50,
@@ -24,6 +38,17 @@ public extension SBKManager {
         return response.tracks.track
     }
     
+    /**
+     Get the most popular tracks on Last.fm by country.
+     
+     - Parameters:
+        - country: A country name, as defined by the ISO 3166-1 country names standard.
+        - limit: The number of results to fetch per page. Defaults to 50.
+        - page: The page number to fetch. Defaults to 1.
+        - completion: A closure to be executed when the request is complete. It returns an optional array of ``SBKTrack`` objects representing the top tracks for the specified country, and an optional error.
+     
+     - Note: For more information, see the [Last.fm API documentation](https://www.last.fm/api/show/geo.getTopTracks).
+     */
     func getTopTracks(
         forCountry country: SBKCountry,
         limit: Int = 50,
