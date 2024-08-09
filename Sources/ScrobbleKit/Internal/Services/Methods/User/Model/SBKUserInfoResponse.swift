52339f3355451e56dc3b36c7bc3b466a78050a97
Tomás Martins <tomas.marti@me.com>
2024-08-09 20:01:46 -0300

    Added Documentation for SBKSimilarTrack and SBKUser

--- Sources/ScrobbleKit/Internal/Services/Methods/User/Model/SBKUserInfoResponse.swift
+++ Sources/ScrobbleKit/Internal/Services/Methods/User/Model/SBKUserInfoResponse.swift
@@ -7,21 +7,53 @@
 
 import Foundation
 
+/// Represents a user on Last.fm, providing various details about their profile and activity.
 public struct SBKUser {
+    /// The username of the Last.fm user.
     public var username: String
+    
+    /// The real name of the user, if provided.
     public var realName: String?
+    
+    /// The profile image of the user.
     public var image: SBKImage?
+    
+    /// The country of the user, if provided.
     public var country: String?
+    
+    /// The age of the user, if provided.
     public var age: String?
+    
+    /// Indicates whether the user has a Last.fm Pro account.
     public var isPro: Bool
+    
+    /// The total number of tracks played by the user.
     public var playcount: Int
+    
+    /// The number of unique artists in the user's library.
     public var artistCount: String?
+    
+    /// The number of playlists created by the user.
     public var playlistsCount: Int?
+    
+    /// The bootstrap value for the user, if available.
     public var bootstrap: String?
+    
+    /// The gender of the user, if provided.
     public var gender: String?
+    
+    /// The URL of the user's Last.fm profile.
     public var url: String
+    
+    /// The type of the user account.
     public var type: String?
+    
+    /// The date when the user joined Last.fm.
     public var memberSince: Date
+    
+    /// Initializes a new `SBKUser` instance from a Last.fm API response.
+    ///
+    /// - Parameter response: The API response containing user information.
     
     init(from response: SBKUserInfoDataResponse) {
         self.username = response.name
--- Sources/ScrobbleKit/Public/Models/SBKSimilarTrack.swift
+++ Sources/ScrobbleKit/Public/Models/SBKSimilarTrack.swift
@@ -7,9 +7,14 @@
 
 import Foundation
 
+/// Represents a track that is similar to another track, as determined by Last.fm.
 public struct SBKSimilarTrack {
-    /// The track that this similar track represents
+    /// The track that this similar track represents.
     public var track: SBKTrack
-    /// The match value for the similarity between the original track and the similar track, as a percentage (from 0.0 to 1.0).
+    
+    /// The match value for the similarity between the original track and this similar track.
+    ///
+    /// This value is a percentage represented as a decimal between 0.0 and 100.0
+    /// A higher value indicates a stronger similarity.
     public var match: Double?
 }
