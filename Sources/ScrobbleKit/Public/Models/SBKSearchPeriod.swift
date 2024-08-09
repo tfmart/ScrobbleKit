db214cde2bbb07e86c4b9d03cf0267719afc8703
Tomás Martins <tomas.marti@me.com>
2024-08-09 20:15:34 -0300

    Added documentation for SBKSearchPeriod

--- Sources/ScrobbleKit/Public/Models/SBKSearchPeriod.swift
+++ Sources/ScrobbleKit/Public/Models/SBKSearchPeriod.swift
@@ -7,11 +7,23 @@
 
 import Foundation
 
+/// Represents the time periods available for Last.fm chart and statistic queries.
 public enum SBKSearchPeriod: String {
+    /// Represents all-time statistics.
     case overall
+    
+    /// Represents statistics for the last 7 days.
     case sevenDays = "7day"
+    
+    /// Represents statistics for the last month.
     case oneMonth = "1month"
+    
+    /// Represents statistics for the last 3 months.
     case threeMonths = "3month"
+    
+    /// Represents statistics for the last 6 months.
     case sixMonths = "6month"
+    
+    /// Represents statistics for the last 12 months.
     case twelveMonths = "12month"
 }
