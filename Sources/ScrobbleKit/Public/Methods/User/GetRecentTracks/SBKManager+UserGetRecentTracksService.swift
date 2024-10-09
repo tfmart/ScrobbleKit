45fe98d88eb66d41422bdd26a85700075761e68f
Tomás Martins <tomas.marti@me.com>
2024-10-09 20:50:04 -0300

    Updated get recent track parameters to use Date instead of TimeInterval

--- Sources/ScrobbleKit/Public/Methods/User/GetRecentTracks/SBKManager+UserGetRecentTracksService.swift
+++ Sources/ScrobbleKit/Public/Methods/User/GetRecentTracks/SBKManager+UserGetRecentTracksService.swift
@@ -15,9 +15,9 @@ public extension SBKManager {
      - user: The username of the user.
      - limit: The number of results to fetch per page. Default value is 50.
      - page: The page number to fetch. Default value is 1.
-     - from: Beginning timestamp of a range in UNIX timestamp format. Default value is nil.
-     - extended: Includes extended data in each artist and whether or not the user has loved each track. Default value is nil.
-     - to: End timestamp of a range in UNIX timestamp format. Default value is nil.
+     - startDate: The start date of the range for which to retrieve tracks. Default value is nil.
+     - extended: Includes extended data in each artist, and whether or not the user has loved each track. Default value is false.
+     - endDate: The end date of the range for which to retrieve tracks. Default value is nil.
      
      - Returns: An asynchronous task that returns an ``SBKSearchResult`` object containing the recent tracks.
      
@@ -25,14 +25,21 @@ public extension SBKManager {
      
      - Note: See [Last.fm's user.getRecentTracks documentation](https://www.last.fm/api/show/user.getRecentTracks) for more information.
      */
-    func getRecentTracks(fromUser user: String, limit: Int = 50, page: Int = 1, from: TimeInterval? = nil, extended: Bool? = nil, to: TimeInterval? = nil) async throws -> SBKSearchResult<SBKScrobbledTrack> {
+    func getRecentTracks(
+        fromUser user: String,
+        limit: Int = 50,
+        page: Int = 1,
+        startDate: Date? = nil,
+        extended: Bool = false,
+        endDate: Date? = nil
+    ) async throws -> SBKSearchResult<SBKScrobbledTrack> {
         let service = UserGetRecentTracksService(
             user: user,
             limit: limit,
             page: page,
-            from: from,
+            from: startDate,
             extended: extended,
-            to: to,
+            to: endDate,
             apiKey: apiKey,
             secretKey: secret
         )
@@ -61,21 +68,29 @@ public extension SBKManager {
      - user: The username of the user.
      - limit: The number of results to fetch per page. Default value is 50.
      - page: The page number to fetch. Default value is 1.
-     - from: Beginning timestamp of a range in UNIX timestamp format. Default value is nil.
-     - extended: Includes extended data in each artist and whether or not the user has loved each track. Default value is nil.
-     - to: End timestamp of a range in UNIX timestamp format. Default value is nil.
+     - startDate: The start date of the range for which to retrieve tracks. Default value is nil.
+     - extended: Includes extended data in each artist, and whether or not the user has loved each track. Default value is false.
+     - endDate: The end date of the range for which to retrieve tracks. Default value is nil.
      - completion: The completion handler to call when the request is complete. It returns an ``SBKSearchResult`` object containing the recent tracks, or an error if the operation fails.
      
      - Note: See [Last.fm's user.getRecentTracks documentation](https://www.last.fm/api/show/user.getRecentTracks) for more information.
      */
-    func getRecentTracks(fromUser user: String, limit: Int = 50, page: Int = 1, from: TimeInterval? = nil, extended: Bool? = nil, to: TimeInterval? = nil, completion: ((SBKSearchResult<SBKScrobbledTrack>?, Error?) -> Void)?) {
+    func getRecentTracks(
+        fromUser user: String,
+        limit: Int = 50,
+        page: Int = 1,
+        startDate: Date? = nil,
+        extended: Bool = false,
+        endDate: Date? = nil,
+        completion: ((SBKSearchResult<SBKScrobbledTrack>?, Error?) -> Void)?
+    ) {
         let service = UserGetRecentTracksService(
             user: user,
             limit: limit,
             page: page,
-            from: from,
+            from: startDate,
             extended: extended,
-            to: to,
+            to: endDate,
             apiKey: apiKey,
             secretKey: secret
         )
