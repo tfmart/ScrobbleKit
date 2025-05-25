2fcbd69d545596c777ed317a253caceaa654a3fe
Tomás Martins <tomas.marti@me.com>
2025-05-25 20:33:37 -0300

    Add FoundationNetworking to SBKService

--- Sources/ScrobbleKit/Internal/Services/Protocols/SBKService.swift
+++ Sources/ScrobbleKit/Internal/Services/Protocols/SBKService.swift
@@ -6,6 +6,9 @@
 //
 
 import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
 
 protocol SBKService: Sendable {
     associatedtype ResponseType: Decodable & Sendable
